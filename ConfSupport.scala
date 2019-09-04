33f8ba6de9ffa2f103fe42836038a848921df826
Gustavo Temple <gustavotemple@users.noreply.github.com>
2019-09-04 10:16:48 -0300

    Update ConfSupport.scala

--- ConfSupport.scala
+++ ConfSupport.scala
@@ -3,7 +3,6 @@ package gatling
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.http.protocol.HttpProtocolBuilder
-import io.gatling.http.request.builder.HttpRequestBuilder
 
 object ConfSupport {
   private val baseUrl = sys.env.getOrElse("BASE_URL", "http://127.0.0.1:8080")
