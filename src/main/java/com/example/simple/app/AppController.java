4515e9c2e68f18b27acf968f8be4ff87c75c8f28
Gustavo Frederico Temple Pedrosa <gustavo.pedrosa@dextra-sw.com>
2019-10-08 20:49:12 -0300

    adding rands

--- src/main/java/com/example/simple/app/AppController.java
+++ src/main/java/com/example/simple/app/AppController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping(path = "/v1/api")
@@ -13,22 +14,39 @@ public class AppController {
 
     @GetMapping
     public ResponseEntity get() {
-        for (int i = 0; i < 200 + getRandomNumberInRange(0, 10); i++) {
-            for (int j = 0; j < 200 + getRandomNumberInRange(0, 10); j++) {
-
+        for (int i = 0; i < 200 + getRandomNumberInRange1(0, 2); i++) {
+            for (int j = 0; j < 200 + getRandomNumberInRange1(0, 2); j++) {
             }
         }
 
         return ResponseEntity.ok().build();
     }
 
-    private static int getRandomNumberInRange(int min, int max) {
-        if (min >= max) {
-            throw new IllegalArgumentException("max must be greater than min");
+    private static int getRandomNumberInRange1(int min, int max) {
+        if (min >= max || (max - min + 1 > Integer.MAX_VALUE)) {
+            throw new IllegalArgumentException("Invalid range");
+        }
+
+        double rand = Math.random();
+        return (int) (rand * ((max - min) + 1)) + min;
+    }
+
+    private static int getRandomNumberInRange2(int min, int max) {
+        if (min >= max || (max - min + 1 > Integer.MAX_VALUE)) {
+            throw new IllegalArgumentException("Invalid range");
         }
 
         Random random = new Random();
         return random.ints(min, (max + 1)).findFirst().getAsInt();
+    }
+
+    private static int getRandomNumberInRange3(int min, int max) {
+        if (min >= max || (max - min + 1 > Integer.MAX_VALUE)) {
+            throw new IllegalArgumentException("Invalid range");
+        }
+
+        return ThreadLocalRandom.current()
+                .nextInt(min, max + 1);
     }
 
 }
